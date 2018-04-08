39ddc2020d89a6695958d33f78fca423852047f3
Renã Oliveira <rena.p.oliveira@gmail.com>
2018-04-08 14:42:22 -0300

    -Increasing session lifetime to 10 hours -Encrypting session data

--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 600),
 
     'expire_on_close' => false,
 
@@ -44,7 +44,7 @@ return [
     |
     */
 
-    'encrypt' => false,
+    'encrypt' => true,
 
     /*
     |--------------------------------------------------------------------------
